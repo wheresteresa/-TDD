edf2b4bec0e4729110a01c33826b929b988c5204
wheresteresa <64266886+wheresteresa@users.noreply.github.com>
2022-05-10 09:06:05 +0800

    Front page HTML now generated froma template

--- function_test_task1.py
+++ function_test_task1.py
@@ -11,6 +11,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -35,9 +36,10 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table=self.browser.find_element_by_id('id_list_table')
-        rouws=table.find_element_by_tag_name('tr')
+        rows=table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1.Buy peacock feathers' for row in rows)
+            any(row.text == '1.Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+	return render(request, 'home.html')
+#from django.http import HttpResponse
 
 # Create your views here.
-def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+#def home_page(request):
+	#return HttpResponse('<html><title>To-Do lists</title></html>')
