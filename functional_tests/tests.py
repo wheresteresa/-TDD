47b2cad118b466ad763a6c212f27f003ceb21e96
wheresteresa <64266886+wheresteresa@users.noreply.github.com>
2022-06-06 13:21:21 +0800

    On branch master message: this is a commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 10
 
@@ -10,6 +11,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser=webdriver.Chrome()
+        staging_server = '39.107.72.190'
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
