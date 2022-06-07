a235bca8d76efe48180b682ad5f2ef7bc61ab568
wheresteresa <64266886+wheresteresa@users.noreply.github.com>
2022-06-07 15:22:59 +0800

    Add gunicorn to virtualenv requirements

--- superlists/settings.py
+++ superlists/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g^+lgvy%2@x4udj%@nmfq*+5cg*9zudrfzp9c(d&r&isyuy-+l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘*’]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
